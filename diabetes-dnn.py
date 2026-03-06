bcc4e244458e1fe97cc1ff7854d287293d9b4a50
aviksha-RVU <aviksha.rvu@gmail.com>
2026-03-06 15:30:28 +0530

    Update diabetes-dnn.py

--- diabetes-dnn.py
+++ diabetes-dnn.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import sklearn
-
+ 
 # Import necessary modules
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
